f89590d2b988e14d9c205fabc64af89b91723b10
mateu <mateu@DESKTOP-E2E99LD>
2025-11-07 16:11:08 +0100

    Dodaj pliki projektów.

---
+++ SDL2_game/SDL2_game.cpp
@@ -0,0 +1,40 @@
+﻿#include <SDL.h>
+
+int main(int argc, char* argv[]) {
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) return 1;
+
+    SDL_Window* window = SDL_CreateWindow(
+        "SDL2",
+        SDL_WINDOWPOS_CENTERED,
+        SDL_WINDOWPOS_CENTERED,
+        640, 480,
+        SDL_WINDOW_SHOWN
+    );
+
+    if (!window) return 1;
+
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+
+    bool running = true;
+    SDL_Event e;
+
+    while (running) {
+        while (SDL_PollEvent(&e)) {
+            if (e.type == SDL_QUIT)
+                running = false;
+        }
+
+        SDL_SetRenderDrawColor(renderer, 30, 30, 30, 255);
+        SDL_RenderClear(renderer);
+
+        SDL_SetRenderDrawColor(renderer, 200, 50, 50, 255);
+        SDL_RenderDrawLine(renderer, 0, 0, 640, 480);
+
+        SDL_RenderPresent(renderer);
+    }
+
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+    return 0;
+}
